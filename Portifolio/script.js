e7131bdcf76fd76276d9b263fb2ef9a16e776d5b
Smil <149633447+IsaiasSorriso@users.noreply.github.com>
2024-07-07 16:56:07 -0300

    tooltips

--- Portifolio/script.js
+++ Portifolio/script.js
@@ -3,8 +3,8 @@ var animacao = document.getElementById('mario');
 animacao.onclick = function () {
     animacao.classList.add('Animação')
     animacao.src = 'imgs/mariopulando.png';
-    const changeTime = 1500;
-    const Time = 3000;
+    const changeTime = 1000;
+    const Time = 2000;
 
     setTimeout(() => {
         animacao.src = 'imgs/marioperdendo.png'; // Troca para a nova imagem
@@ -17,5 +17,10 @@ animacao.onclick = function () {
 
     
 }
-const exampleEl = document.getElementById('example')
-const tooltip = new bootstrap.Tooltip(exampleEl, options)
+
+document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+  });
